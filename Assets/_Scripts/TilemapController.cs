bef4717e7ca0fadef4293a7534d80a3558ad24f5
ismailtk48 <ismailtk48@gmail.com>
2023-03-08 16:56:49 +0300

    Implemented get methods and change color methods for individual Tiles.

--- Assets/_Scripts/TilemapController.cs
+++ Assets/_Scripts/TilemapController.cs
@@ -3,18 +3,78 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
+    public Camera mainCamera;
     public Tilemap tilemap;
+    public TileBase[] tiles; // An array of Tiles in the Tileset
+    [SerializeField] private GameObject _player;
+    Vector3 nextposition;
+
+
 
     void Start()
     {
-        foreach (Vector3Int position in tilemap.cellBounds.allPositionsWithin)
+        mainCamera = Camera.main;
+        BoundsInt bounds = tilemap.cellBounds;
+        tiles = new TileBase[bounds.size.x * bounds.size.y * bounds.size.z];
+        for (int z = bounds.min.z; z < bounds.max.z; z++)
         {
-            TileBase tile = tilemap.GetTile(position);
-            if (tile != null)
+            for (int y = bounds.min.y; y < bounds.max.y; y++)
             {
-                // Do something with the tile
-                Debug.Log("Found tile at position " + position);
+                for (int x = bounds.min.x; x < bounds.max.x; x++)
+                {
+                    TileBase tile = tilemap.GetTile(new Vector3Int(x, y, z));
+                    int index = (z - bounds.min.z) * bounds.size.x * bounds.size.y + (y - bounds.min.y) * bounds.size.x + (x - bounds.min.x);
+                    tiles[index] = tile;
+                }
             }
         }
     }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeColorOfTileAtMouseClick();
+        }
+    }
+    public TileBase GetTileByIndex(int index)
+    {
+        // Check that the index is within the range of the tiles array
+        if (index >= 0 && index < tiles.Length)
+        {
+            return tiles[index];
+        }
+        else
+        {
+            Debug.LogError("Invalid tile index: " + index);
+            return null;
+        }
+    }
+
+
+    public void ChangeColorOfTileAtMouseClick()
+    {
+        Vector3 tilePosToChangeColor = GetTilePositionAtMouseClick();
+        Vector3Int tilePosition = tilemap.WorldToCell(tilePosToChangeColor);
+        tilemap.SetTileFlags(tilePosition, TileFlags.None);
+        tilemap.SetColor(tilePosition, Color.red);
+    }
+
+    public Vector3 GetTilePositionAtMouseClick()
+    {
+        Vector3 mousePosScreen = Input.mousePosition;
+        Vector3 nextposition = mainCamera.ScreenToWorldPoint(mousePosScreen);
+        Vector3Int nextTilePosition = tilemap.WorldToCell(nextposition);
+        Vector3 destination = tilemap.GetCellCenterWorld(nextTilePosition);
+        return destination;
+    }
+
+    public TileBase GetTileAtPosition(Vector3Int tilePosition)
+    {
+        TileBase tile = tilemap.GetTile(tilePosition);
+        return tile; 
+    }
+
+
 }
