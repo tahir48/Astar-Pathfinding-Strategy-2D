2a03c04fc76409aa1335bf4512446b988caea51b
ismailtk48 <ismailtk48@gmail.com>
2023-03-13 17:46:49 +0300

    Barracks can be deselected.

--- Assets/_Scripts/Buildings/MilitaryBuilding.cs
+++ Assets/_Scripts/Buildings/MilitaryBuilding.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace StrategyGame_2DPlatformer
@@ -7,17 +8,51 @@ namespace StrategyGame_2DPlatformer
     {
         public bool isPlaced;
         #region Selection Related Variables
-        [SerializeField] private Image barracksImage;
-        [SerializeField] private Image soldierImage;
+        private Image buildingsImageUI;
+        private Image soldierImageUI;
         [SerializeField] private Sprite sprite;
+        [SerializeField] private Sprite nullImage;
+        [SerializeField] private Sprite swordsmanSprite;
+        private SpriteRenderer _spriteRender;
         bool isSelected = false;
         #endregion
+
+
+        private void Start()
+        {
+            buildingsImageUI = GameObject.FindGameObjectWithTag("BuildingImage").GetComponent<Image>();
+            soldierImageUI = GameObject.FindGameObjectWithTag("Soldiers").GetComponent<Image>();
+            _spriteRender = GetComponent<SpriteRenderer>();
+        }
+        void Update()
+        {
+            if (isSelected && Input.GetMouseButtonDown(0))
+            {
+                // Check if the clicked object is not the building itself
+                if (!EventSystem.current.IsPointerOverGameObject() && !IsClickOnBuilding())
+                {
+                    // Deselect the building
+                    isSelected = false;
+                    _spriteRender.color = Color.white;
+                    buildingsImageUI.sprite = nullImage;
+                    soldierImageUI.sprite = nullImage;
+                }
+            }
+        }
+
+        bool IsClickOnBuilding()
+        {
+            // Check if the mouse position is inside the building sprite
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return _spriteRender.bounds.Contains(mousePosition);
+        }
+
 
         #region Selection Related Functionality
         public void OnBarracksClicked()
         {
-            barracksImage = GameObject.FindGameObjectWithTag("BuildingImage").GetComponent<Image>();
-            barracksImage.sprite = sprite;
+            buildingsImageUI.sprite = sprite;
+            soldierImageUI.sprite = swordsmanSprite;
         }
 
         void OnMouseDown()
@@ -25,7 +60,7 @@ namespace StrategyGame_2DPlatformer
             if (isPlaced)
             {
                 isSelected = true;
-                GetComponent<SpriteRenderer>().color = Color.red;
+                _spriteRender.color = Color.red;
                 OnBarracksClicked();
             }
         }
